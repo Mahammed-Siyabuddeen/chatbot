05ef9b1d5cc6e93b1f6b49f11c2b4c479549311b
Mahammed-Siyabuddeen <mohd.sihab615@gmail.com>
2024-12-27 22:35:09 +0530

    responsive

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [loading,setLoading]=useState("")
   return (
     <div className="bg-slate-800 h-dvh text-white ">
-      <div className="w-3/5 h-full m-auto  flex flex-col justify-between">
+      <div className="w-3/5 max-md:w-11/12 h-full m-auto  flex flex-col justify-between">
         <Result result={result} loading={loading}/>
         <Prompt setResult={setResult} loading={loading} setLoading={setLoading}/>
       </div>
